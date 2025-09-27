a7623f785912efa994b03d201063f1cd24dd7013
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-09-27 22:20:45 +0600

    add JavaScript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+const cityNameInput = document.getElementById("cityName");
+
+getWeatherBtn.addEventListener("click", searchBtnFunc);
+
+function searchBtnFunc() {
+	const cityName = cityNameInput.value;
+	if (cityName) {
+		// TODO: Fetch the weather with the city name
+	} else {
+		console.log("Please enter the city name");
+	}
+}
